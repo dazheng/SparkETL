058020b0a57861268d6df9d730a44623d370b1a0
cuixuezheng <cuixuezheng@hotmail.com>
2020-03-16 23:05:52 +0800

    delete old files

--- etl/src/main/resources/export/dp_d.sql
+++
@@ -1,3 +0,0 @@
-delete from m_holiday;
-@m_holiday
-select holiday_type, cnt from dm.m_holiday;
--- etl/src/main/resources/transform/s2m_d.sql
+++
@@ -1,4 +0,0 @@
-insert overwrite table dm.m_holiday
-select holiday_type, count(1)
-from stg.s_holiday
-group by holiday_type;
